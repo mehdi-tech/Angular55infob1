f6f6dffe57766a642b3a7d94dd8a983c17e00f59
mehdi-tech <mehdi.hemdani@esprit.tn>
2020-09-22 12:41:41 +0200

    tp1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'myFirstProjects';
-}
+  first: number = 0;
+  tab: Array<string> = ['Angular', 'React', 'Node'];
+  constructor() { }
+  ngOnInit() {
+    this.first = this.first + 1;
+    console.log(this.first);
+    for (let i in this.tab) {
+      if (this.tab[i] === 'Node'){console.log('Nodepour le Backend'); }
+      else if (this.tab[i] === 'React'){console.log('React pour le front'); }
+      else {(console.log('Angular pour le front')); }
+    }
+}}
